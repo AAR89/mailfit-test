b07d8c3b330fe348fb629863e09a65c5ba5f4c38
Alex Ur <ru.booking@yandex.ru>
2024-07-17 19:51:42 +0300

    add final layout without media

---
+++ main.js
@@ -0,0 +1,9 @@
+const submitBtn = document.querySelector('.banner_submit-button')
+const socmediaLinks = document.querySelectorAll('.social-media-link');
+
+function handleClick() {
+    console.log('submit button clicked');
+}
+
+submitBtn.addEventListener('click', handleClick);
+
